0e660e660d0e6e89f3adc4763badacf0fc48ee2e
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-15 11:56:52 -0800

    Add random scope to cat and dog models.

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,3 +1,4 @@
 class Cat < ActiveRecord::Base
   validates :name, :breed, :age, :gender, :bio, :presence => true
+  scope :random, -> { order('RANDOM()').first }
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,4 @@
 class Dog < ActiveRecord::Base
   validates :name, :breed, :age, :gender, :bio, :presence => true
+  scope :random, -> { order('RANDOM()').first }
 end
