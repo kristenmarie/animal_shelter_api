3e2eb0dc7fd2691942a94e197816defa88330a9a
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-15 09:14:56 -0800

    Add dogs controller.

---
+++ app/controllers/api/vi/dogs_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::DogsController < Api::V1::ApiController
+  def index
+    @dogs = Dog.all
+    json_response(@dogs)
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    json_response(@dogs)
+  end
+
+  def create
+    @dog = Dog.create!(dog_params)
+    json_response(@dog, :created)
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    if @dog.update!(dog_params)
+      render status: 200, json: {
+        message: "This dog has been updated successfully."
+      }
+    end
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    @dog.destroy
+  end
+
+
+  private
+
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def dog_params
+    params.permit(:name, :breed, :age, :gender, :declawed, :location)
+  end
+end
