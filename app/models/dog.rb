c189af8b3f481ad26774c8753de88eac3f5b6770
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-15 12:22:32 -0800

    Add search scopes to cat and dog models.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,7 @@
 class Dog < ActiveRecord::Base
   validates :name, :breed, :age, :gender, :bio, :presence => true
   scope :random, -> { order( 'RANDOM()' ).first }
+  scope :breed_search, -> (breed){ where('breed = ?', breed) }
+  scope :age_search, -> (age){ where('age = ?', age) }
+  scope :gender_search, -> (gender){ where('gender = ?', gender) }
 end
