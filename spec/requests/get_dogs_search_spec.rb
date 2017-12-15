960f697fa4ab412d6a648030e64a7e12475c0884
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-15 13:24:53 -0800

    Add get dog from search spec.

---
+++ spec/requests/get_dogs_search_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'get dog from search route', type: :request do
+  let!(:dogs) { FactoryBot.create_list(:dog, 20) }
+  let!(:test_dog) { FactoryBot.create(:dog, breed: 'Malamute')}
+
+  # before do
+  #   get "/api/v1/cats?breed=#{dogs.first.breed}"
+  # end
+  #
+  # it "returns malamute dogs" do
+  #   dog_returned = JSON.parse(response.body)
+  #   expect(dog_returned['breed']).to eq(dogs.first.breed)
+  # end
+
+  before do
+    get "/api/v1/dogs?breed=Malamute"
+  end
+
+  it 'returns malamute dogs' do
+    expect(JSON.parse(response.body).size).to eq(1)
+  end
+
+  it "returns status 200" do
+    expect(response).to have_http_status(:success)
+  end
+end
