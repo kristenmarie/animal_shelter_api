d79402df43074a7d21aac0c7bde809a0996ec287
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-20 11:52:41 -0800

    Fix spec test in get dogs search request.

--- spec/requests/get_dogs_search_spec.rb
+++ spec/requests/get_dogs_search_spec.rb
@@ -4,14 +4,14 @@ describe 'get dog from search route', type: :request do
   let!(:dogs) { FactoryBot.create_list(:dog, 20) }
   let!(:test_dog) { FactoryBot.create(:dog, breed: 'Malamute')}
 
-  # before do
-  #   get "/api/v1/cats?breed=#{dogs.first.breed}"
-  # end
-  #
-  # it "returns malamute dogs" do
-  #   dog_returned = JSON.parse(response.body)
-  #   expect(dog_returned['breed']).to eq(dogs.first.breed)
-  # end
+  before do
+    get "/api/v1/cats?breed=#{dogs.first.breed}"
+  end
+
+  it "returns malamute dogs" do
+    dog_returned = JSON.parse(response.body)
+    expect(dog_returned[0]['breed']).to eq(test_dog.breed)
+  end
 
   before do
     get "/api/v1/dogs?breed=Malamute"
